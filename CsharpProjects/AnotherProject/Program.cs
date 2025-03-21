9b1e7f790f58e0e03d55b1b8c7737557bac0d77a
swordsounds <126785544+swordsounds@users.noreply.github.com>
2025-03-21 20:03:47 -0230

    Thing

---
+++ CsharpProjects/AnotherProject/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new();
+
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 10;
+
+if (daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire sooon. Renew now!");
+}
+
+else if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {random} days.");
+    Console.WriteLine($"Renew  now and save {discountPercentage}%!");
+}
+
+else if (daysUntilExpiration <= 1) {
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage * 2}");
+}
+
+else {
+    Console.WriteLine("Your subscription has expried.");
+}
--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random dice = new();
-int roll = dice.Next(1,7);
-Console.WriteLine(roll);
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = System.Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
