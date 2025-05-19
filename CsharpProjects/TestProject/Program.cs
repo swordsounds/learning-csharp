40e9d232ed04e52800460c850767bceb2a6270c4
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-05-19 19:38:10 -0230

    SDK dev.

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,25 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Random random = new Random();
-string[] orderIDs = new string[5];
-// Loop through each blank orderID
-for (int i = 0; i < orderIDs.Length; i++)
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] studentScores = new int[10];
+
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
 {
-    // Get a random value that equates to ASCII letters A through E
-    int prefixValue = random.Next(65, 70);
-    // Convert the random value into a char, then a string
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    // Create a random number, pad with zeroes
-    string suffix = random.Next(1, 1000).ToString("000");
-    // Combine the prefix and suffix together, then assign to current OrderID
-    orderIDs[i] = prefix + suffix;
-}
-// Print out each orderID
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
 
 // To create project and src files - dotnet new console -o ./CsharpProjects/TestProject
-// To run from cd - otnet run --project .\CsharpProjects\TestProject\TestProject.csproj
-// 
+// To run from current dir - dotnet run --project .\CsharpProjects\TestProject\TestProject.csproj
+// To build from current dir - dotnet build .\CsharpProjects\TestProject\TestProject.csproj
